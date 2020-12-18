2a0147e3ff299d67398aa447eb5a4715dbf8b15d
Kelvin De Los Angeles <kelvin623@gmail.com>
2020-12-18 15:13:35 +0300

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,182 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { motion, useAnimation } from "framer-motion";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home = () => {
+    const blackOverlayControl = useAnimation();
+    const SloganControl = useAnimation();
+    const ComingSoonControl = useAnimation();
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const sloganWrapperVariants = {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.7,
+            },
+        },
+    };
+    const sloganVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+    };
+    const comingSoonWrapperVariant = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                duration: 1.5,
+            },
+        },
+    };
+    const comingSoonVariant = {
+        logo: {
+            y: 0,
+            transition: {
+                duration: 1.5,
+            },
+        },
+        text: {
+            opacity: 1,
+            transition: {
+                duration: 1.5,
+                delay: 1.5,
+            },
+        },
+    };
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    useEffect(() => {
+        const sequence = async () => {
+            await blackOverlayControl.start({
+                y: 0,
+                transition: {
+                    duration: 1,
+                    easings: "easeIn",
+                    delay: 1.3,
+                },
+            });
+            await SloganControl.start("visible");
+            await SloganControl.start({ opacity: 0, transition: { duration: 1 } });
+            await ComingSoonControl.start("visible");
+        };
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        sequence();
+    }, []);
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    return (
+        <Component>
+            <BlackOverlay initial={{ y: "-100%" }} animate={blackOverlayControl} />
+            <Slogan initial={"hidden"} animate={SloganControl} variants={sloganWrapperVariants}>
+                <motion.p variants={sloganVariants}>Design</motion.p>
+                <motion.p variants={sloganVariants}>With</motion.p>
+                <motion.p variants={sloganVariants}>Us</motion.p>
+            </Slogan>
+            <Page initial={"hidden"} variants={comingSoonWrapperVariant} animate={ComingSoonControl}>
+                <main>
+                    <motion.img src='/assets/images/logo/logo_white.svg' alt='' initial={{ y: -50 }} variants={comingSoonVariant} animate={"logo"} />
+                    {/* <motion.p className='bio' initial={{ opacity: 0 }} variants={comingSoonVariant} animate={"text"}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad
+                    </motion.p> */}
+                    <motion.p className='coming-soon' initial={{ opacity: 0 }} variants={comingSoonVariant} animate={"text"}>
+                        Coming Soon
+                    </motion.p>
+                </main>
+                <motion.footer initial={{ opacity: 0 }} variants={comingSoonVariant} animate={"text"}>
+                    <a href='mailto:sinan@wu.com.tr'>sinan@wu.com.tr</a>
+                    <a href='tel:+90 0530 220 34 20'>+90 0530 220 34 20</a>
+                    <a href='mailto:guven@wu.com.tr'>guven@wu.com.tr</a>
+                </motion.footer>
+            </Page>
+        </Component>
+    );
+};
+const Component = styled.div`
+    position: relative;
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
+    width: 100vw;
+    /* ========= */
+`;
+const BlackOverlay = styled(motion.div)`
+    position: absolute;
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
+    width: 100vw;
+    background-color: black;
+    z-index: 1;
+`;
+const Slogan = styled(motion.div)`
+    position: absolute;
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
+    width: 100vw;
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    grid-column-gap: 8px;
+    justify-content: center;
+    align-content: center;
+    z-index: 2;
+    p {
+        color: white;
+        text-transform: uppercase;
+        font-weight: 300;
+    }
+`;
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+const Page = styled(motion.div)`
+    position: absolute;
+    padding: 32px 8px;
+    width: 100vw;
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
+    display: grid;
+    grid-template-rows: 1fr auto;
+    z-index: 3;
+    main {
+        display: grid;
+        justify-items: center;
+        align-items: center;
+        align-content: center;
+        grid-row-gap: 40px;
+        p {
+            color: white;
+            max-width: 494px;
+            text-align: center;
+        }
+        .logo {
+            width: 60%;
+            max-width: 267px;
+        }
+        .bio {
+            font-weight: 300;
+            line-height: 24px;
+        }
+        .coming-soon {
+            font-size: 16px;
+            font-weight: 500;
+            @media (min-width: 768px) {
+                font-size: 20px;
+            }
+        }
+    }
+    footer {
+        display: grid;
+        grid-row-gap: 8px;
+        justify-items: center;
+        grid-template-columns: repeat(3, 1fr);
+        grid-column-gap: 8px;
+    }
+    a {
+        color: white;
+        font-size: 12px;
+        font-weight: 300;
+        @media (min-width: 768px) {
+            font-size: 16px;
+        }
+    }
+`;
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default Home;
